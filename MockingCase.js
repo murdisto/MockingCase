59fb48ece7de8f52f6662b7bba5f24993a9afc3c
Scott Ian <sim2487@gmail.com>
2020-02-18 19:15:34 -0800

    First commit.

---
+++ MockingCase.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt');
+
+prompt.start().get(['Input'], (err, result) => {
+  if (err) { return onErr(err); }
+  let strArr = result.Input.split("");
+  
+  const toMockingCase = strArr => {
+    let newArr = [];
+    for (let i = 0; i < strArr.length; i++) {
+      if (i % 2 === 0) {        
+        newArr.push(strArr[i].toUpperCase());
+      }else {
+        newArr.push(strArr[i]);
+      }
+    }
+    return newArr;
+  };
+  
+  let output = toMockingCase(strArr).join('');
+  console.log(output);
+});
+
+function onErr(err) {
+  console.log(err);
+  return 1;
+}
