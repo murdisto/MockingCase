db117783e43c3a115873d6abf43874fc1209ffe1
Scott Ian <sim2487@gmail.com>
2020-02-24 13:47:24 -0800

    Minor style change. Add Copy to Clipboard button and functionality.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const button = document.getElementById("button");
+const submitButton = document.getElementById("submit-button");
 const onButtonClick = () => {
   const mockInput = document.getElementById("mock-input").value;
   const toMockingCase = mockInput => {
@@ -16,14 +16,31 @@ const onButtonClick = () => {
   };
 
   document.getElementById("result").textContent = "";
-  document.getElementById("result").textContent += toMockingCase(mockInput);
+  document.getElementById("result").textContent = toMockingCase(mockInput);
   document.getElementById("mock-input").value = "";
 };
 
-button.addEventListener("click", onButtonClick);
+submitButton.addEventListener("click", onButtonClick);
 document.getElementById("mock-input").addEventListener("keypress", e => {
   if (e.code === "Enter") {
     e.preventDefault();
     onButtonClick();
   }
 });
+
+const copyButton = document.getElementById("copy-button");
+copyButton.addEventListener("click", e => {
+  e.preventDefault();
+  copyText(document.getElementById("result").textContent);  
+});
+
+const copyText = elementId => {
+  let copiedText = elementId;
+  const tempInputField = document.createElement("input");
+  tempInputField.type = "text";
+  tempInputField.value = copiedText;
+  document.body.appendChild(tempInputField);
+  tempInputField.select();
+  document.execCommand("Copy");
+  document.body.removeChild(tempInputField);
+};
