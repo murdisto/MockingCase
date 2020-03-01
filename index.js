35e4aff0098d2aa54db812397e2f5e35993d6b7a
Scott Ian <sim2487@gmail.com>
2020-02-29 20:50:47 -0800

    Code clean up.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const submitButton = document.getElementById("submit-button");
+const submitButton = document.getElementById('submit-button');
 const onButtonClick = () => {
-  const mockInput = document.getElementById("mock-input").value;
-  const toMockingCase = mockInput => {
-    let strArr = mockInput.split("");
+  const mockInput = document.getElementById('mock-input').value;
+  const toMockingCase = input => {
+    const strArr = input.split('');
     const newArr = [];
     for (let i = 0; i < strArr.length; i++) {
       if (i % 2 === 0) {
@@ -11,38 +11,38 @@ const onButtonClick = () => {
         newArr.push(strArr[i]);
       }
     }
-    let output = newArr.join("");
+    const output = newArr.join('');
     return output;
   };
 
-  document.getElementById("result").textContent = "";
-  document.getElementById("result").textContent = toMockingCase(mockInput);
-  document.getElementById("mock-input").value = "";
-  document.getElementById("output").style.visibility = "visible";
+  document.getElementById('result').textContent = '';
+  document.getElementById('result').textContent = toMockingCase(mockInput);
+  document.getElementById('mock-input').value = '';
+  document.getElementById('output').style.visibility = 'visible';
 };
 
-submitButton.addEventListener("click", onButtonClick);
-document.getElementById("mock-input").addEventListener("keypress", e => {
-  if (e.code === "Enter") {
+submitButton.addEventListener('click', onButtonClick);
+document.getElementById('mock-input').addEventListener('keypress', e => {
+  if (e.code === 'Enter') {
     e.preventDefault();
     onButtonClick();
   }
 });
 
-const copyButton = document.getElementById("copy-button");
-copyButton.addEventListener("click", e => {
-  e.preventDefault();
-  copyText(document.getElementById("result").textContent);  
-  copyButton.textContent = "Copied to Clipboard!";  
-});
-
 const copyText = elementId => {
-  let copiedText = elementId;
-  const tempInputField = document.createElement("input");
-  tempInputField.type = "text";
+  const copiedText = elementId;
+  const tempInputField = document.createElement('input');
+  tempInputField.type = 'text';
   tempInputField.value = copiedText;
   document.body.appendChild(tempInputField);
   tempInputField.select();
-  document.execCommand("Copy");
+  document.execCommand('Copy');
   document.body.removeChild(tempInputField);
 };
+
+const copyButton = document.getElementById('copy-button');
+copyButton.addEventListener('click', e => {
+  e.preventDefault();
+  copyText(document.getElementById('result').textContent);
+  copyButton.textContent = 'Copied to Clipboard!';
+});
